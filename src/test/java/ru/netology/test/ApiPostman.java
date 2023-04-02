41ca94c0848a94e7ff87082bfb4009ca9d5814d7
Nyrik1992 <mirgaleeva92@mail.ru>
2023-04-02 22:49:05 +0600

    fataly test

--- src/test/java/ru/netology/test/ApiPostman.java
+++ src/test/java/ru/netology/test/ApiPostman.java
@@ -1,2 +1,35 @@
-package ru.netology.test;public class ApiPostman {
+package ru.netology.test;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
+public class ApiPostman {
+
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+
+                .body("[1].currency", equalTo("USD"))
+                .body("[0].currency", equalTo("RUS"))
+        ;
+
+
+    }
 }
+
+
+
